b305089982befd82454df5bce2c79fb671c5341e
leedeukgyo <dg1411@naver.com>
2022-07-20 09:30:23 +0900

    [fe] 리액트 생성, 게임방 화면

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Gameroom from './components/Gameroom'
+
+function App() {
+  return (
+    <div className="App">
+      <Gameroom/>
+    </div>
+  );
+}
+
+export default App;
