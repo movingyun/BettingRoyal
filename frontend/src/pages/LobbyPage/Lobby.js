6737d1bdac73c8c4b9ffb99cbfb8965285633645
leedeukgyo <dg1411@naver.com>
2022-07-25 01:50:17 +0900

    sockjs socket open

--- frontend/src/pages/LobbyPage/Lobby.js
+++ frontend/src/pages/LobbyPage/Lobby.js
@@ -5,24 +5,46 @@ import Box from "@mui/material/Box";
 import Sidemenu from "../../components/lobbycomp/Sidemenu";
 import Noticeboard from "../Noticepage/Noticeboard";
 import Rooms from "../../components/lobbycomp/Rooms";
-import Rank from "../Rankpage/Rank"
+import Rank from "../Rankpage/Rank";
+import Sockjs from "sockjs-client";
 
 export default function Lobby(props) {
   useEffect(() => {}, []);
-  
+
+  function socketConnect() {
+    let socket = new Sockjs("(proxy)/url");
+
+    socket.onopen = function () {
+      //소켓 전송
+
+      //소켓 수신
+      socket.onmessage = function (event) {
+        var contents = JSON.parse(event.data);
+        console.log("Type:", contents.type);
+        console.log("ReceivedMessage:", contents);
+
+        messageType(contents);
+      };
+    };
+  }
+
+  function socketSend() {}
+
+  function messageType(contents) {}
+
   let sidemenu = (
     <Grid container>
       <Grid item container xs={3}>
         <Sidemenu />
       </Grid>
       <Grid item container xs={9}>
-          <Routes>
-          <Route path="rooms" element ={<Rooms/>} ></Route>
-            <Route path="noticeboard" element ={<Noticeboard/>} ></Route>
-            <Route path="rank" element={<Rank/>}></Route>
-            <Route path="friend" ></Route>
-            <Route path="mypage" ></Route>
-          </Routes>
+        <Routes>
+          <Route path="rooms" element={<Rooms />}></Route>
+          <Route path="noticeboard" element={<Noticeboard />}></Route>
+          <Route path="rank" element={<Rank />}></Route>
+          <Route path="friend"></Route>
+          <Route path="mypage"></Route>
+        </Routes>
       </Grid>
     </Grid>
   );
--- frontend/src/pages/Rankpage/Rank.js
+++ frontend/src/pages/Rankpage/Rank.js
@@ -20,23 +20,41 @@ export default function Rooms(props) {
   const columns = [
     { field: "id", headerName: "순위", width: 90 },
     {
+        field: "tier",
+        headerName: "티어",
+        width: 30,
+        editable: false,
+      },
+    {
       field: "nickname",
       headerName: "닉네임",
       width: 150,
       editable: false,
     },
     {
+        field: "badge",
+        headerName: "뱃지",
+        width: 30,
+        editable: false,
+      },
+    {
       field: "ruby",
       headerName: "보유 루비",
       width: 150,
       editable: false,
-    }
+    },
+    {
+        field: "guild",
+        headerName: "길드명",
+        width: 150,
+        editable: false,
+      }
   ];
 
   const rows = [
-    { id: 1, nickname: "유저1", ruby: 100,},
-    { id: 2, nickname: "유저2", ruby: 10, },
-    { id: 3, nickname: "유저3", ruby: 1, },
+    { id: 1, tier: "금",nickname: "유저1", badge:"다이아",ruby: 100, guild:"길드1"},
+    { id: 2, tier: "은",nickname: "유저2",  badge:"다이아",ruby: 10, guild:"길드2"},
+    { id: 3, tier: "동",nickname: "유저3",  badge:"다이아",ruby: 1, guild:"길드2"},
     
   ];
 
