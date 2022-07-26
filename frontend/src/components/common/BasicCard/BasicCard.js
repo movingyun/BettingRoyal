7dff8b8aca27bc1b19f23208fde66f36195e129f
유동윤 <ydy1107@naver.com>
2022-07-26 15:53:45 +0900

    merge test

---
+++ frontend/src/components/common/BasicCard/BasicCard.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const BasicCard = () => {
+    return (
+        <div style={{marginLeft: '500px'}}>
+        공지사항
+        </div>
+    )
+}
+
+export default BasicCard
---
+++ frontend/src/pages/Mypage/Mypage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Mypage = () => {
+    return (
+        <div style={{marginLeft: '700px'}}>
+            This is Mypage page.
+        </div>
+    )
+}
+
+export default Mypage
