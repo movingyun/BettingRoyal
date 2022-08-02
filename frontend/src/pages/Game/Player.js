ee447724243a9b10d28f189c784e1493f35e3745
박수민 <suminies@gmail.com>
2022-08-02 19:26:13 +0900

    [FE] Design : Gameroom

--- frontend/src/pages/Game/Player.js
+++ frontend/src/pages/Game/Player.js
@@ -1,5 +1,8 @@
+import { Divider } from "@material-ui/core";
 import { useState, useEffect } from "react";
 import styles from "./Player.module.css";
+import card_back from "../../images/cards/card_back.png";
+import card_am_1 from "../../images/cards/card_am_1.png";
 
 export default function Player(props) {
   const [player, setPlayer] = useState(props.player);
@@ -20,9 +23,9 @@ export default function Player(props) {
   }, [props.player]);
 
   return (
-    <div>
+    <div className={styles.container}>
       {player ? (
-        <div className={styles.Player}>
+        <div className={styles.player}>
           <div className="face">{player.face}</div>
           <div className="name">{player.name}</div>
           <div className="ruby">{player.ruby}</div>
@@ -31,8 +34,37 @@ export default function Player(props) {
           <div className="status">{player.status}</div>
         </div>
       ) : (
-        <div>
-          <div class="empty"> + </div>
+        // 플레이어 안들어옴
+        // <div className={styles.player}>
+        //   <div className={styles.empty}> + </div>
+        // </div>
+
+        // 플레이어 들어왔을 때 테스트
+        <div className={styles.player}>
+          <div className={styles.list}>
+            <div className={styles.camera}>카메라</div>
+            <div className={styles.info}>
+              <div className={styles.userInfo}>
+                  <div className={styles.name}>닉네임</div>
+                  <div className={styles.ruby}>100,000 루비</div>
+              </div>
+              {/* <hr className={styles.divider}/> */}
+              <div className={styles.gameInfo}>
+                <div className={styles.card}>
+                    {/* 카드뒷면 */}
+                    <div className={styles.card_back}>
+                        <img src={card_back} />
+                    </div>
+                    {/* 카드앞면오픈 */}
+                    <div className={styles.card_front}>
+                        <img src={card_am_1} />
+                    </div>
+                </div>
+                <div className={styles.status}>더블</div>
+                <div className={styles.emotion}>감정상태</div>
+              </div>
+            </div>
+          </div>
         </div>
       )}
     </div>
