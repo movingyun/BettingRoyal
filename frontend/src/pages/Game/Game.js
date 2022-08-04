1a9ec74c52d5defb7f49f0660fc644ef1f368dfd
leedeukgyo <dg1411@naver.com>
2022-08-04 18:16:40 +0900

    [FE] Feat : 소켓 수신 간소화, 필요 데이터 기록

--- frontend/src/pages/Game/Game.js
+++ frontend/src/pages/Game/Game.js
@@ -7,6 +7,7 @@ import sockjs from "sockjs-client";
 import stompjs from "stompjs";
 import axios from "axios";
 import { listClasses } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 import MicRoundedIcon from "@mui/icons-material/MicRounded";
 import VideocamRoundedIcon from "@mui/icons-material/VideocamRounded";
@@ -15,12 +16,19 @@ export default function Game(props) {
   const [playerOpenvidu, setPlayerOpenvidu] = useState([]);
   const [players, setPlayers] = useState([]);
   //player { socketId, nickname, ruby, bet, myCard, pair}
-  const [myCard, setMyCard] = useState([]);
+  //const [myCard, setMyCard] = useState([]);
+  const [currentBetUnit, setCurrentBetUnit] = useState([]);
+  const [currentMaxBet, setCurrentMaxBet] = useState([]);
+  const [myBet, setMyBet] = useState([]);
+  const [myTotalBet, setMyTotalBet] = useState([]);
+
   const [totalBet, setTotalBet] = useState([]);
   const [groundCard, setGroundCard] = useState([]);
 
+  let navigate = useNavigate();
   let roomId = ""; //방 컴포넌트에 roomid 포함
   let stomp;
+
   useEffect(() => {
     var sock = new sockjs("http://localhost:8080");
     stomp = stompjs.over(sock);
@@ -33,50 +41,50 @@ export default function Game(props) {
 
       //메시지를 받으면
       stomp.subscribe("/sub/game/room" + roomId, function (message) {
-        
         //content.type, content.message등으로 사용 가능
         var content = JSON.parse(message.body);
 
-
         //참가 후 정보받기
         if (content.type == "PLAYERSINFO") {
+          //{players[], betUnit}
           //받은 player[0]이 자신
           setPlayers(content.playersInfo);
-          setPlayerOpenvidu(content.playerOpenvidu)
-          //playerinfo {  nickname, ruby  }
+          setPlayerOpenvidu(content.playerOpenvidu);
         }
-
 
         //사람이 들어왔을 때
         if (content.type == "ENTER") {
+          //{player}
           console.log("사람들어왔다" + message.body);
           setPlayers([...players, content.playerInfo]);
-          //playerinfo {  nickname, ruby  }
         }
 
         //사람이 나갔을 때
         if (content.type == "ENTER") {
+          //{player}
           let leftPlayer = players.find((player) => player.socketId == content.socketId);
           setPlayers(players.filter((player) => player.socketId != content.socketId));
           console.log(leftPlayer.nickname + " left");
-          //playerinfo {  nickname, ruby  }
         }
 
         //게임이 시작됐을 때
         if (content.type == "START") {
-          console.log("게임시작하자" + content.message);
+          //ui 게임ui로 바뀜. 버튼생성
           //카메라 체크
           //기본베팅
         }
 
         //그라운드 카드 받을 때
         if (content.type == "GROUNDCARD") {
+          //{groundCard[]}
           console.log("그라운드카드 받아라~" + content.message);
           setGroundCard(content.groundCard);
         }
 
-        //기본 배팅 하자
+        /*
+        // 배팅 하자
         if (content.type == "UNITBETTING") {
+          //
           //setPlayers(content.playerInfo);
           
           setPlayers(players.map(
@@ -86,11 +94,14 @@ export default function Game(props) {
           ))
           setTotalBet(totalBet+content.bet)
         }
+*/
 
-        //카드 받자 player수만큼 요청하자
+        //플레이어 카드 받기
         if (content.type == "MAKECARDSET") {
-          console.log("개인카드 달라고 요청했음");
-          //기본배팅 했으니까 GETCARD 요청
+          //{players[]}
+
+          setPlayers(content.message.playersInfo);
+          /*
           stomp.send(
             "/pub/game/message",
             {},
@@ -101,36 +112,86 @@ export default function Game(props) {
               type: "GETMYCARD",
             })
           );
+          */
         }
 
-        //카드 만들어서 요청 넘어왔으면 찍어주자
-        if (content.type == "GETMYCARD") {
-          console.log("개인카드 받았다.");
-          //무슨 카드를 받았나?
+        //턴
+        if (content.type == "TURN") {
+          //{socketId}
+          let turnplayer = players.find((player) => player.socketId == content.socketId);
+
+          //플레이어 하이라이트, 내턴이면 버튼 활성화
+          //내턴일때
+          if (currentMaxBet > myTotalBet) {
+            //콜 버튼 비활성화
+          }
+
+        }
+
+        //수시로 서버와 동기화
+        if (content.type == "SYNC") {
+          //{betUnit, players[], }
+          setPlayers(content.playersInfo);
+          setCurrentBetUnit(content.betUnit);
         }
       });
     });
 
-    return ()=>{
+    return () => {
       //컴포넌트 unmount 시
       stomp.send(
         "/pub/game/message",
         {},
-        JSON.stringify({ roomId: roomId, message: "", sender: "" , type: "EXIT"})
+        JSON.stringify({ roomId: roomId, message: "", sender: "", type: "EXIT" })
       );
-    }
+    };
   }, []);
 
-  function sendCall() {
+  //콜 다이 레이즈 올인 클릭
+  function sendBet(action) {
     //call die raise allin
-    console.log("보내기 눌렀다.--------");
-    var content = "";
-    var nickname = "";
-    stomp.send(
-      "/pub/game/message",
-      {},
-      JSON.stringify({ roomId: roomId, message: content, sender: nickname })
-    );
+
+    switch (action) {
+      case "call":
+        console.log("call");
+        stomp.send(
+          "/pub/game/message",
+          {},
+          JSON.stringify({ roomId: roomId, message: "", sender: "", type: "CALL" })
+        );
+
+        break;
+      case "raise":
+        console.log("raise " + myBet);
+        stomp.send(
+          "/pub/game/message",
+          {},
+          JSON.stringify({ roomId: roomId, message: myBet, sender: "", type: "RAISE" })
+        );
+
+        break;
+      case "allin":
+        console.log("allin ");
+        stomp.send(
+          "/pub/game/message",
+          {},
+          JSON.stringify({ roomId: roomId, message: "", sender: "", type: "ALLIN" })
+        );
+
+        break;
+      case "die":
+        console.log("die");
+        stomp.send(
+          "/pub/game/message",
+          {},
+          JSON.stringify({ roomId: roomId, message: "", sender: "", type: "DIE" })
+        );
+        //나가기 버튼 활성화
+
+        break;
+      default:
+        break;
+    }
   }
 
   function startGame() {
@@ -144,6 +205,17 @@ export default function Game(props) {
     );
   }
 
+  //나가기
+  function leaveGame() {
+    console.log("나가기 누름");
+
+    stomp.send(
+      "/pub/game/message",
+      {},
+      JSON.stringify({ roomId: roomId, message: "", sender: "", type: "EXIT" })
+    );
+    navigate("../lobby");
+  }
 
   //클라이언트 webrtc socket on:
   //클라이언트 game socket emit : bet, call, raise, die , 나가기, 참가,
