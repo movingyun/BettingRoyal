1a1585474e879afa09a4da291ec384928c0c3e02
sung <janfar@naver.com>
2022-08-09 13:44:22 +0900

    test

--- frontend/src/pages/Notice/Notice.js
+++ frontend/src/pages/Notice/Notice.js
@@ -1,9 +1,33 @@
-import React from "react"
+import React, {useEffect, useState, useRef} from "react";
+import axios from "axios";
 
 const Notice = () => {
+    const [nickname, setNickname] = useState();
+
     return (
+        // <div style={{marginLeft: '500px'}}>
+        // 공지사항<br/>
+        // <input type="text" placeholder="제목" className="" ></input>
+        // </div>
         <div style={{marginLeft: '500px'}}>
-        공지사항
+            <h1>공지사항 목록</h1>
+            <div>
+                <table>
+                <thead>
+                    <tr>
+                        <th>No.</th>
+                        <th>제목</th>
+                        <th>작성자</th>
+                        <th>작성일자</th>
+                        <th>조회수</th>
+                        <th>좋아요수</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    
+                </tbody>
+                </table>
+            </div>
         </div>
     )
 }
