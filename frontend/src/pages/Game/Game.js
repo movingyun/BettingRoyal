040bd927a7f5364ae2e151fdbed05dff21de4bc0
leedeukgyo <dg1411@naver.com>
2022-08-12 04:30:45 +0900

    [FE] Fix : 클라이언트 소켓아이디 보유로 변경, 우승시 효과 추가

--- frontend/src/pages/Game/Game.js
+++ frontend/src/pages/Game/Game.js
@@ -8,7 +8,7 @@ import stompjs from "stompjs";
 import axios from "axios";
 import { listClasses } from "@mui/material";
 import { useNavigate, useLocation } from "react-router-dom";
-
+import ruby_win from "../../images/ruby_win.gif";
 import MicRoundedIcon from "@mui/icons-material/MicRounded";
 import VideocamRoundedIcon from "@mui/icons-material/VideocamRounded";
 import card_am_1 from "../../images/cards/card_am_1.png";
@@ -26,12 +26,14 @@ export default function Game(props) {
   const [myTotalBet, setMyTotalBet] = useState(0);
   const [mainMessage, setmainMessage] = useState("");
   const [buttonDisable, setbuttonDisable] = useState([true, true, true, true]);
-  const [startDisabled, setstartDisabled] = useState(true);
+  const [startDisabled, setstartDisabled] = useState(false);
   const [gameTotalBet, setGameTotalBet] = useState(0);
   const [groundCard1, setGroundCard1] = useState(0);
   const [groundCard2, setGroundCard2] = useState(0);
   const [roomInfo, setroomInfo] = useState();
-  const [isEnter,setisEnter]=useState(false);
+  const [isEnter, setisEnter] = useState(false);
+  const [sessionId, setsessionId] = useState(false);
+  const [win, setwin] = useState([false,false,false,false,false,false]);
 
   let navigate = useNavigate();
   let location = useLocation();
@@ -41,12 +43,15 @@ export default function Game(props) {
 
   useEffect(() => {
     console.log(roomId + "번 방 참가");
-
     stomp.connect({}, () => {
       stomp.send(
         "/pub/game/message",
         {},
-        JSON.stringify({ roomId: roomId, senderNickName: window.localStorage.getItem("accessToken"), type: "ENTER" })
+        JSON.stringify({
+          roomId: roomId,
+          senderNickName: window.localStorage.getItem("accessToken"),
+          type: "ENTER",
+        })
       );
 
       //메시지를 받으면
@@ -82,17 +87,23 @@ export default function Game(props) {
           setGroundCard2(content.groundCardNum2);
         }
 
-
-        
-
         //게임 끝
         if (content.type == "GAMEEND") {
           setbuttonDisable([true, true, true, true]);
+          setTurn(content.turnIdx);
+          //이긴사람 표시
+          let tempwin = win;
+          tempwin[content.turnIdx]=true;
+          setwin(tempwin)
 
-          //5초 기다림
-          setInterval(() => {}, 500);
+          //2.5초간 효과재생 후 게임시작 활성화
+          setTimeout(() => {
+            setwin([false,false,false,false,false,false])
+            setIsStart(false);
+          }, 2500);
 
-          setIsStart(false);
+
+          
         }
 
         //수시로 서버와 동기화
@@ -103,36 +114,36 @@ export default function Game(props) {
         }
       });
 
-
       stomp.subscribe("/user/sub/game/room" + roomId, function (message) {
         var content = JSON.parse(message.body);
 
         //사람이 들어왔을 때
         if (content.type == "ENTER") {
           console.log("사람들어왔다" + JSON.stringify(content.playerInfo));
+          setTurn(content.turnIdx);
           if (content.playerInfo) {
             setPlayers(content.playerInfo);
+            setsessionId(content.playerInfo[0].sessionId);
           }
           setroomInfo(content);
           setisEnter(true);
-          if(content.turnIdx==0){
-            setstartDisabled(false)
+          if (content.turnIdx == 0) {
+            setstartDisabled(false);
           }
         }
 
         //개인 카드 받기
         if (content.type == "GETMYCARD") {
-          setPlayers(content.playerInfo)
-          if(content.turnIdx==0){
-            setbuttonDisable([false,true,false,false])
+          setPlayers(content.playerInfo);
+          if (content.turnIdx == 0) {
+            setbuttonDisable([false, true, false, false]);
           }
-          setCurrentBetUnit(content.battingUnit)
-
-
+          setCurrentBetUnit(content.battingUnit);
         }
 
         //턴
         if (content.type == "NEXTTURN") {
+          setTurn(content.turnIdx);
           //내턴일때
           if (content.turnIdx == 0) {
             setbuttonDisable([false, false, false, false]);
@@ -146,18 +157,20 @@ export default function Game(props) {
             setbuttonDisable([true, true, true, true]);
           }
         }
-
-
-
       });
     });
-
     return () => {
       //컴포넌트 unmount 시
       stomp.send(
         "/pub/game/message",
         {},
-        JSON.stringify({ roomId: roomId, message: "", sender: "", type: "EXIT" })
+        JSON.stringify({
+          roomId: roomId,
+          message: "",
+          sender: "",
+          type: "EXIT",
+          socketId: sessionId,
+        })
       );
     };
   }, []);
@@ -175,14 +188,18 @@ export default function Game(props) {
   }, [sec]);
 
   function gameStart(e) {
-    console.log("겜시작");
-    setIsStart(true);
+    if (turn == 0) {
+      console.log("겜시작");
+      setIsStart(true);
 
-    stomp.send(
-      "/pub/game/message",
-      {},
-      JSON.stringify({ roomId: roomId, message: "", sender: "", type: "START" })
-    );
+      stomp.send(
+        "/pub/game/message",
+        {},
+        JSON.stringify({ roomId: roomId, message: "", sender: "", type: "START" })
+      );
+    } else {
+      console.log("방장만 시작 가능");
+    }
 
     // // 타이머 시작
     // timerId.current = setInterval(() => {
@@ -205,7 +222,13 @@ export default function Game(props) {
         stomp.send(
           "/pub/game/message",
           {},
-          JSON.stringify({ roomId: roomId, message: "", sender: "", type: "CALL" })
+          JSON.stringify({
+            roomId: roomId,
+            message: "",
+            sender: "",
+            type: "CALL",
+            socketId: sessionId,
+          })
         );
 
         break;
@@ -215,7 +238,13 @@ export default function Game(props) {
         stomp.send(
           "/pub/game/message",
           {},
-          JSON.stringify({ roomId: roomId, message: currentBetUnit, sender: "", type: "RAISE" })
+          JSON.stringify({
+            roomId: roomId,
+            message: currentBetUnit,
+            sender: "",
+            type: "RAISE",
+            socketId: sessionId,
+          })
         );
 
         break;
@@ -224,7 +253,13 @@ export default function Game(props) {
         stomp.send(
           "/pub/game/message",
           {},
-          JSON.stringify({ roomId: roomId, message: "", sender: "", type: "ALLIN" })
+          JSON.stringify({
+            roomId: roomId,
+            message: "",
+            sender: "",
+            type: "ALLIN",
+            socketId: sessionId,
+          })
         );
 
         break;
@@ -233,7 +268,13 @@ export default function Game(props) {
         stomp.send(
           "/pub/game/message",
           {},
-          JSON.stringify({ roomId: roomId, message: "", sender: "", type: "DIE" })
+          JSON.stringify({
+            roomId: roomId,
+            message: "",
+            sender: "",
+            type: "DIE",
+            socketId: sessionId,
+          })
         );
         //나가기 버튼 활성화
 
@@ -250,7 +291,13 @@ export default function Game(props) {
       stomp.send(
         "/pub/game/message",
         {},
-        JSON.stringify({ roomId: roomId, message: "", sender: "", type: "EXIT" })
+        JSON.stringify({
+          roomId: roomId,
+          message: "",
+          sender: "",
+          type: "EXIT",
+          socketId: sessionId,
+        })
       );
       navigate("../lobby");
     } else {
@@ -307,24 +354,31 @@ export default function Game(props) {
         </div>
 
         <div className={styles.player1}>
+          {win[1] ? <img src={ruby_win} /> : null}
+          
           <Player player={players[1]} />
         </div>
         <div className={styles.player2}>
+        {win[2] ? <img src={ruby_win} /> : null}
           <Player player={players[2]} />
         </div>
         <div className={styles.player3}>
+        {win[3] ? <img src={ruby_win} /> : null}
           <Player player={players[3]} />
         </div>
         <div className={styles.player4}>
+        {win[4] ? <img src={ruby_win} /> : null}
           <Player player={players[4]} />
         </div>
         <div className={styles.player5}>
+        {win[5] ? <img src={ruby_win} /> : null}
           <Player player={players[5]} />
         </div>
         <div className={styles.playerMe}>
+        {win[0] ? <img src={ruby_win} /> : null}
           <Player player={players[0]} />
-        </div> 
-        {/* {isEnter ? <GameOpenvidu roomId = {roomId} roomInfo={roomInfo}/> : null} 
+        </div>
+        {/* {isEnter ? <GameOpenvidu roomId = {roomId} roomInfo={roomInfo}/> : null}
          */}
         {isStart ? (
           <div className={styles.betting}>
