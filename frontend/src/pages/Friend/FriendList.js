75316433af67bab1c3466e57a7ec11ca919e476d
sung <janfar@naver.com>
2022-08-15 15:23:07 +0900

    [FE] Fix : FriendList alert

--- frontend/src/pages/Friend/FriendList.js
+++ frontend/src/pages/Friend/FriendList.js
@@ -134,6 +134,7 @@ export default function Friends(props) {
           <button className={styles.remove}
             onClick={() => {
               deleteFriendBtn(params);
+              alert("친구를 삭제했습니다.");
             }}
           >
           <DeleteRoundedIcon className={styles.icon}/> 친구삭제
