5924477555abc4c65be968725ee7d8ba6fd27506
지용현 <yung5487@gmail.com>
2022-08-16 16:46:18 +0900

    [FE] Fix : rooms

--- frontend/src/pages/Rooms/Rooms.js
+++ frontend/src/pages/Rooms/Rooms.js
@@ -7,48 +7,54 @@ import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import DialogContentText from "@mui/material/DialogContentText";
-import DialogTitle from "@mui/material/DialogTitle"; 
+import DialogTitle from "@mui/material/DialogTitle";
 import TextField from "@mui/material/TextField";
 import Box from "@mui/material/Box";
 import { DataGrid } from "@mui/x-data-grid";
 import styles from "./Rooms.module.css";
-import {makeStyles} from "@material-ui/core/styles";
-import {createStyles} from "@material-ui/core";
-import AddCircleOutlineRoundedIcon from '@mui/icons-material/AddCircleOutlineRounded';
-import { styled } from '@mui/material/styles';
-import Paper from '@mui/material/Paper';
-import Pagination from '@mui/material/Pagination';
-import Stack from '@mui/material/Stack';
-import ArrowForwardIosRoundedIcon from '@mui/icons-material/ArrowForwardIosRounded';
-import PersonRoundedIcon from '@mui/icons-material/PersonRounded';
+import { makeStyles } from "@material-ui/core/styles";
+import { createStyles } from "@material-ui/core";
+import AddCircleOutlineRoundedIcon from "@mui/icons-material/AddCircleOutlineRounded";
+import { styled } from "@mui/material/styles";
+import Paper from "@mui/material/Paper";
+import Pagination from "@mui/material/Pagination";
+import Stack from "@mui/material/Stack";
+import ArrowForwardIosRoundedIcon from "@mui/icons-material/ArrowForwardIosRounded";
+import PersonRoundedIcon from "@mui/icons-material/PersonRounded";
 
 const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: '#DCD7C9',
-  padding: '10px 15px 10px 15px',
-  height: '105px',
-  borderRadius: '5px',
-  boxShadow: 'none',
+  backgroundColor: "#DCD7C9",
+  padding: "10px 15px 10px 15px",
+  height: "105px",
+  borderRadius: "5px",
+  boxShadow: "none",
   fontFamily: "'Noto Sans KR', sans-serif",
-  '&:hover': {
-    backgroundColor: '#A27B5C',
+  "&:hover": {
+    backgroundColor: "#A27B5C",
   },
 }));
 
 export default function Rooms(props) {
-  const useStyles = makeStyles((theme) => createStyles({
-    grid: {
-      fontFamily: "'Noto Sans KR', sans-serif",
-      fontSize: '16px',
-      fontWeight: '400',
-    },    
-  }));
+  const useStyles = makeStyles((theme) =>
+    createStyles({
+      grid: {
+        fontFamily: "'Noto Sans KR', sans-serif",
+        fontSize: "16px",
+        fontWeight: "400",
+      },
+    })
+  );
 
   const style = useStyles();
 
+  const [allRooms, setAllRooms] = useState([]);
   const [rooms, setRooms] = useState([]);
   const [makeRoomTitle, setMakeRoomTitle] = useState("");
   const [makeRoomBettingunit, setMakeRoomBettingunit] = useState();
+  const [roomCnt, setRoomCnt] = useState();
   const [makeRoomPw, setMakeRoomPw] = useState("");
+  const [page, setPage] = useState(1);
+
   let navigate = useNavigate();
 
   useEffect(() => {
@@ -63,8 +69,9 @@ export default function Rooms(props) {
         //[{roomId, userm, roomTitle, roomBettingUnit, roomPw}, ... ]
         //console.log(JSON.stringify(response.data));
         // return JSON.stringify(response.data.statusCode);
-
-        setRooms(makeRoomList(response.data));
+        setAllRooms(response.data);
+        setRoomCnt(response.data.length);
+        setRooms(makeRoomList(response.data, 1));
       })
       .catch(function (error) {
         alert("방 정보 가져오기 실패");
@@ -74,6 +81,12 @@ export default function Rooms(props) {
   let fetchedRooms = <Grid container></Grid>;
 
   const [open, setOpen] = useState(false);
+
+  const handleChange = (event, value) => {
+    setPage(value);
+    setRooms(makeRoomList(allRooms, value));
+    console.log(rooms);
+  };
 
   const handlemakeroom = () => {
     setOpen(true);
@@ -100,13 +113,13 @@ export default function Rooms(props) {
       .then(function (response) {
         //console.log(JSON.stringify(response))
         setOpen(false);
-        navigate("/room", { state: { roomId:response.data.roomId } });
+        navigate("/room", { state: { roomId: response.data.roomId } });
       })
       .catch(function (error) {});
   }
 
-  function enterRoom(e){
-    navigate("/room", { state: { roomId:e.id } })
+  function enterRoom(e) {
+    navigate("/room", { state: { roomId: e.id } });
     //console.log(e.id);
   }
 
@@ -123,7 +136,7 @@ export default function Rooms(props) {
   let roomcreate = (
     <Grid item xs={12}>
       <button className={styles.createBtn} onClick={handlemakeroom}>
-      <AddCircleOutlineRoundedIcon sx={{ fontSize: 20, mr:0.5, mb:0.4 }} />방 만들기
+        <AddCircleOutlineRoundedIcon sx={{ fontSize: 20, mr: 0.5, mb: 0.4 }} />방 만들기
       </button>
       <Dialog
         open={open}
@@ -132,12 +145,8 @@ export default function Rooms(props) {
         aria-describedby="alert-dialog-description"
       >
         <DialogContent className={styles.createModal}>
-          <div className={styles.modalTitle}>
-            방 만들기
-          </div>
-          <div className={styles.modalExplain}>
-            방장이 되어 게임에 참여하세요!
-          </div>
+          <div className={styles.modalTitle}>방 만들기</div>
+          <div className={styles.modalExplain}>방장이 되어 게임에 참여하세요!</div>
           <div className={styles.modalInput}>
             <TextField
               color="action"
@@ -164,130 +173,68 @@ export default function Rooms(props) {
               className={styles.modalField}
             />
           </div>
-          <button className={styles.okBtn} onClick={makeRoom}>확인</button>
+          <button className={styles.okBtn} onClick={makeRoom}>
+            확인
+          </button>
         </DialogContent>
       </Dialog>
     </Grid>
   );
 
-  function makeRoomList(roomsdata) {
+  function makeRoomList(roomsdata, value) {
+    console.log(page + "여기ㅣㅣㅣㅣㅣㅣㅣ");
     let list = [];
-    for (let i = 0; i < roomsdata.length; i++) {
+    for (let i = 8 * (value - 1); i < 8 * value; i++) {
+      if (i >= roomCnt) {
+        continue;
+      }
       let room = new Object();
-      room.id = roomsdata[i].roomId;
+      room.id = roomsdata[i].id;
       room.roomTitle = roomsdata[i].roomTitle;
       room.roomBettingUnit = roomsdata[i].roomBettingUnit;
       room.isPw = roomsdata[i].roomPw;
-      room.current_count = 1;
-      room.max_count = 1;
-      console.log(JSON.stringify(room));
+      room.current_count = roomsdata[i].roomInCnt;
+      room.max_count = 6;
+      console.log(JSON.stringify(roomsdata[i]));
       list.push(room);
     }
     return list;
   }
-  //[{roomId, user, roomTitle, roomBettingUnit, roomPw}, ... ]
-  const columns = [
-    {
-      field: "id",
-      headerName: "방 번호",
-      flex: 1,
-      align: "center",
-      headerAlign: "center",
-    },
-    {
-      field: "roomTitle",
-      headerName: "방 제목",
-      flex: 4,
-      editable: false,
-      align: "center",
-      headerAlign: "center",
-    },
-    {
-      field: "roomBettingUnit",
-      headerName: "루비 베팅 단위",
-      flex: 1.5,
-      editable: false,
-      align: "center",
-      headerAlign: "center",
-    },
-    {
-      field: "current_count",
-      headerName: "현재인원",
-      flex: 1,
-      editable: false,
-      align: "center",
-      headerAlign: "center",
-    },
-    {
-      field: "max_count",
-      headerName: "정원",
-      type: "number",
-      flex: 1,
-      editable: false,
-      align: "center",
-      headerAlign: "center",
-    },
-    {
-      field: "isPw",
-      headerName: "비밀",
-      sortable: false,
-      flex: 1,
-      editable: false,
-      align: "center",
-      headerAlign: "center",
-    },
-  ];
 
   let roomsdummy = (
-    <Grid >
+    <Grid>
       {roomcreate}
-      <Box sx={{ height: 500, width: "100%" }}>
-        <DataGrid
-        className={style.grid}
-          onRowClick={enterRoom}
-          rows={rooms}
-          columns={columns}
-          pageSize={10}
-          rowsPerPageOptions={[5]}
-          disableSelectionOnClick
-        />
-      </Box>
-      <Grid container rowSpacing={'10px'} columnSpacing={'10px'}>
-        <Grid item xs={6}>
-          <Item>
-            <div className={styles.roomNo}>No. 1111</div>
-            <div className={styles.roomName}>방제목</div>
-            <ArrowForwardIosRoundedIcon className={styles.roomIcon} />
-            <hr className={styles.roomDiv}/>
-            <div className={styles.roomBot}>
-              <div className={styles.betRuby}>최소 배팅 10 루비</div>
-              <div className={styles.peopleNum}><PersonRoundedIcon sx={{ fontSize: 20, mr:0.5, mb:0.4 }}/>3/6</div>
-            </div>
-          </Item>
-        </Grid>
-        <Grid item xs={6}>
-          <Item>2</Item>
-        </Grid>
-        <Grid item xs={6}>
-          <Item>3</Item>
-        </Grid>
-        <Grid item xs={6}>
-          <Item>4</Item>
-        </Grid>
-        <Grid item xs={6}>
-          <Item>5</Item>
-        </Grid>
-        <Grid item xs={6}>
-          <Item>6</Item>
-        </Grid>
-        <Grid item xs={6}>
-          <Item>7</Item>
-        </Grid>
-        <Grid item xs={6}>
-          <Item>8</Item>
-        </Grid>
+      <Grid container rowSpacing={"10px"} columnSpacing={"10px"}>
+        {rooms.map((item, index) => (
+          <Grid item xs={6}>
+            <Item
+              onClick={() => {
+                enterRoom(item.id);
+              }}
+            >
+              <div className={styles.roomNo}>{item.id}</div>
+              <div className={styles.roomName}>{item.roomTitle}</div>
+              <ArrowForwardIosRoundedIcon className={styles.roomIcon} />
+              <hr className={styles.roomDiv} />
+              <div className={styles.roomBot}>
+                <div className={styles.betRuby}>최소 배팅 {item.roomBettingUnit} 루비</div>
+                <div className={styles.peopleNum}>
+                  <PersonRoundedIcon sx={{ fontSize: 20, mr: 0.5, mb: 0.4 }} />
+                  {item.current_count}/6
+                </div>
+              </div>
+            </Item>
+          </Grid>
+        ))}
       </Grid>
-      <div className={styles.pageDiv}><Pagination className={styles.page} count={10} /></div>
+      <div className={styles.pageDiv}>
+        <Pagination
+          className={styles.page}
+          count={roomCnt % 8 == 0 ? parseInt(roomCnt / 8) : parseInt(roomCnt / 8 + 1)}
+          onChange={handleChange}
+          page={page}
+        />
+      </div>
     </Grid>
   );
 
